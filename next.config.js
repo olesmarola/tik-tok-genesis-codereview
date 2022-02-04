5b2b4b25a28916078c852dfa513bea06a3e839ce
olesmarola <o.marola@ewm.swiss>
2022-02-04 17:45:32 +0200

    - add config for the es-build - add bundle analyzer plugin

--- next.config.js
+++ next.config.js
@@ -1,5 +1,28 @@
+const { ESBuildMinifyPlugin } = require('esbuild-loader');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+function useEsbuildMinify(config, options) {
+  const terserIndex = config.optimization.minimizer.findIndex(
+    (minimizer) => minimizer.constructor.name === 'TerserPlugin'
+  );
+  if (terserIndex > -1) {
+    config.optimization.minimizer.splice(terserIndex, 1, new ESBuildMinifyPlugin(options));
+  }
+}
+
+function useEsbuildLoader(config, options) {
+  const jsLoader = config.module.rules.find((rule) => rule.test && rule.test.test('.ts'));
+
+  if (jsLoader) {
+    jsLoader.use.loader = 'esbuild-loader';
+    jsLoader.use.options = options;
+  }
+}
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   images: {
     domains: [
@@ -15,4 +38,20 @@ module.exports = {
       'p77-sign-sg-lite.tiktokcdn.com',
     ],
   },
-};
+  webpack: (config, { webpack }) => {
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        React: 'react',
+      })
+    );
+
+    useEsbuildMinify(config);
+
+    useEsbuildLoader(config, {
+      loader: 'tsx',
+      target: 'es2015',
+    });
+
+    return config;
+  },
+});
